55f7fc9e7fd027977ba8cbbad7b621c6c799343c
Ashmaawyy <al_ashmawy@outlook.com>
2024-01-16 20:45:37 +0200

    added update_book_by_id : )

--- routes.py
+++ routes.py
@@ -19,7 +19,7 @@ def create_book(request: Request, book: Book = Body(...)):
 
 @router.get("/", response_description = "List all books", response_model = List[Book])
 def list_books(request: Request):
-    books = list(request.app.database["books"].find(limit=100))
+    books = list(request.app.database["books"].find(limit = 100))
     return books
 
 
@@ -28,4 +28,24 @@ def find_book_by_id(id: str, request: Request):
     if (book := request.app.database["books"].find_one({"_id": id})) is not None:
         return book
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,\
+                        detail = f"Book with ID {id} not found")
+
+@router.put("/{id}", response_description="Update a book", response_model=Book)
+def update_book_by_id(id: str, request: Request, book: BookUpdate = Body(...)):
+    book = {k: v for k, v in book.model_dump().items() if v is not None}
+    if len(book) >= 1:
+        update_result = request.app.database["books"].update_one(
+            {"_id": id}, {"$set": book}
+        )
+
+        if update_result.modified_count == 0:
+            raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,\
+                                detail = f"Book with ID {id} not found")
+
+    if (
+        existing_book := request.app.database["books"].find_one({"_id": id})
+    ) is not None:
+        return existing_book
+
+    raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,\
                         detail = f"Book with ID {id} not found")
